ebbb38ae4f763ed95fb1e271e3685a2e052be285
Saheed Oladele <sdrfngg640@gmail.com>
2020-02-04 16:36:07 +0100

    Add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def create
+    byebug
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/create'
+  # get 'users/create'
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
