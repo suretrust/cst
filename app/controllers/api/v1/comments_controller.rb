de33c09f4fea43274a88606d327866e0201dfb44
Saheed Oladele <sdrfngg640@gmail.com>
2020-02-08 10:57:21 +0100

    Add comment controller

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Api::V1::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors
+    end
+  end
+
+  def index
+    @comments = Comment.all
+    render json: @comments
+  end
+
+  def comment_params
+    params.permit(:user_email, :user_id, :message, :ticket_id)
+  end
+end
