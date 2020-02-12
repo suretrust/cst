65950cdb9e30089a2fb56cd59a5bfaa63b9d133e
Saheed Oladele <sdrfngg640@gmail.com>
2020-02-12 05:45:03 +0100

    Update app for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery except: :create
+
+  private
+
+  def encode_token(payload = {})
+    exp = 24.hours.from_now
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'])
+  end
 end
